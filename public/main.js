ef733138fd2d59c3587c0f6bad9cfff9ae0b59c2
Tim Cai <tim.j.cai@gmail.com>
2023-04-25 00:50:30 +1000

    changed the editing mode to display a completely white div, also added in focus on title when first entering the editor

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@ const completebtn = document.querySelectorAll('.completed')
 const title = document.querySelectorAll('todo.title')
 const tag = document.querySelectorAll('todo.tag')
 const trashcan = document.querySelectorAll('a.delete')
-const listItem = document.querySelectorAll('.list-item')
+const taskBlock = document.querySelectorAll('.task-block')
 const edit = document.querySelectorAll('a.edit')
 
 newTask.addEventListener('click', (e) => {
@@ -15,33 +15,34 @@ newTask.addEventListener('click', (e) => {
 trashcan.forEach((item) => {
   item.addEventListener('click', (e) => {
     const endpoint = `${item.dataset.id}`
-    console.log(endpoint)
     fetch(endpoint, { method: 'DELETE' })
       .then((response) => window.location.reload())
       .catch((err) => console.log(err))
   })
 })
 
-listItem.forEach((item) => {
+taskBlock.forEach((item) => {
   const edit = item.querySelector('.edit')
   edit.addEventListener('click', (e) => {
     item.classList.toggle('editing')
     const itemInputs = item.querySelectorAll('input')
     itemInputs.forEach((input) => {
       input.toggleAttribute('readonly')
-      console.log(input.value)
+      const title = item.querySelector('.title')
+      title.focus()
+      var val = title.value //store the value of the element
+      title.value = '' //clear the value of the element
+      title.value = val //set that value back.
     })
     if (item.classList.contains('editing')) {
       const edit = item.querySelector('.edit')
-      edit.innerHTML = '<i class="ph-fill ph-bookmark-simple"></i>'
+      edit.innerHTML = '<i class="ph-fill ph-bookmark-simple black"></i>'
       edit.addEventListener('click', (e) => {
         const endpoint = `${edit.dataset.id}`
-        console.log(endpoint)
         const body = {
           title: `${item.querySelector('.title').value}`,
           tag: `${item.querySelector('.tag').value}`,
         }
-        console.log(body)
         fetch(endpoint, {
           method: 'PATCH',
           body: JSON.stringify(body),
@@ -63,7 +64,6 @@ completebtn.forEach((item) => {
     const endpoint = `${item.dataset.id}`
     const state = `${item.dataset.state}`
     const body = { completed: state }
-    console.log(body)
     fetch(endpoint, {
       method: 'PATCH',
       body: JSON.stringify(body),
